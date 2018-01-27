6d85b5c44361d396f6f28f4a3a7ffc8d7b071d41
Travis CI <travis@travis-ci.org>
2018-01-27 09:31:11 +0000

    rebuild pages at 3e7fb6d

--- search/functions_3.js
+++ search/functions_3.js
@@ -1,5 +1,15 @@
 var searchData=
 [
+  ['dbg_5fdebugger',['dbg_Debugger',['../debug_8h.html#a4a2a29de41d421710026489dccb3fa43',1,'debug.h']]],
+  ['dbg_5fremoveallbreakpoints',['dbg_RemoveAllBreakpoints',['../debug_8h.html#a6298ae433a481ed122d273c4636a0822',1,'debug.h']]],
+  ['dbg_5fremoveallwatchpoints',['dbg_RemoveAllWatchpoints',['../debug_8h.html#a56e0514c18d113c220dbf5ca53de9426',1,'debug.h']]],
+  ['dbg_5fremovebreakpoint',['dbg_RemoveBreakpoint',['../debug_8h.html#adb920530a0c0a7f870c4cd5b2e640298',1,'debug.h']]],
+  ['dbg_5fremovewatchpoint',['dbg_RemoveWatchpoint',['../debug_8h.html#a093468136b1156ea95e0055a0f116cf0',1,'debug.h']]],
+  ['dbg_5fsetbreakpoint',['dbg_SetBreakpoint',['../debug_8h.html#a1d7dd499187d368cf1de3e120d4f60c1',1,'debug.h']]],
+  ['dbg_5fsetreadwatchpoint',['dbg_SetReadWatchpoint',['../debug_8h.html#acc3b4b7577465e345ff7edec5a6fccd9',1,'debug.h']]],
+  ['dbg_5fsetreadwritewatchpoint',['dbg_SetReadWriteWatchpoint',['../debug_8h.html#a5313602487f608a2e6590fc46efd6783',1,'debug.h']]],
+  ['dbg_5fsetwatchpoint',['dbg_SetWatchpoint',['../debug_8h.html#a11cafa6f91c4da5b153b921b199bf2e5',1,'debug.h']]],
+  ['dbg_5fsetwritewatchpoint',['dbg_SetWriteWatchpoint',['../debug_8h.html#aa22fce30ae85b7231bad22c2e7faaac4',1,'debug.h']]],
   ['delay',['delay',['../tice_8h.html#acfd12579679c85d5f1c716509b2587f6',1,'tice.h']]],
   ['dzx7_5fstandard',['dzx7_Standard',['../decompress_8h.html#a9f3e6d13cfc6d576ea0f67395059e243',1,'decompress.h']]],
   ['dzx7_5fturbo',['dzx7_Turbo',['../decompress_8h.html#a877e3af3164d88fd78f461116d85094e',1,'decompress.h']]]
