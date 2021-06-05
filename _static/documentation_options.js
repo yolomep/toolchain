8a532d4352fa4f2f8e3254737a6bbaa05e1a072c
mateoconlechuga <matthewwaltzis@gmail.com>
2021-06-05 01:01:07 +0000

    Deploying to gh-pages from  @ f6d8652e3e2c9f3f7282cc5bf7b713b7610cba70 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v9.0-devel',
+    VERSION: '',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
